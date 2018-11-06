e631d39c9cf7454b1e9b7399d9f01a296b8a07f2
Guilherme Rodrigues <guiihpr@gmail.com>
2018-11-06 14:13:08 -0200

    Renomeando nomes dos arquivos

--- src/models/has_pain.js
+++ src/models/has_pain.js
@@ -1,39 +1,38 @@
 const Sequelize = require('sequelize')
 const Pain = require('./pain')
 
-const Hospital = global.sequelize.define('has_pain', {
+const HasPain = global.sequelize.define('has_pain', {
   id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    pain_id: {
-      type: Sequelize.INTEGER
-    },
-    appointment_id: {
-      type: Sequelize.INTEGER
-    },
-    createdAt: {
-      type: Sequelize.DATE
-    },
-    updatedAt: {
-      type: Sequelize.DATE
-    },
-    deletedAt: {
-      type: Sequelize.DATE
-    }
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
   },
-  {
-    paranoid: true,
-    tableName: 'appointment_has_pain'
+  pain_id: {
+    type: Sequelize.INTEGER
+  },
+  appointment_id: {
+    type: Sequelize.INTEGER
+  },
+  createdAt: {
+    type: Sequelize.DATE
+  },
+  updatedAt: {
+    type: Sequelize.DATE
+  },
+  deletedAt: {
+    type: Sequelize.DATE
   }
-);
+},
+{
+  paranoid: true,
+  tableName: 'appointment_has_pain'
+}
+)
 
-Hospital.hasMany(Pain, {
+HasPain.hasMany(Pain, {
   as: 'pain',
   foreignKey: 'id',
   sourceKey: 'pain_id'
-});
+})
 
-
-module.exports = Hospital
+module.exports = HasPain
