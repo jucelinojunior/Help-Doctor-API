f1b6bbca660a3770e72df5e501ed73a3f2a5c3b1
Rafael Pinheiro Gonçalves <rafael@Mac-mini-de-rafael.local>
2018-11-07 11:54:12 -0200

    ver hospital, documentação

---
+++ src/handlers/get-hospital.handler.js
@@ -0,0 +1,18 @@
+/**
+ * Rota repsonsável pela criação de usuários
+ * https://imasters.com.br/devsecops/encriptando-senhas-com-o-bcrypt
+ */
+const hospitalService = require('../services/hospital.service')
+
+module.exports = {
+  method: 'GET',
+  path: '/hospital/{id}',
+  handler: async (request) => {
+    return hospitalService.findById(request.params.id);
+  },
+  options: {
+    cors: {
+      origin: ['*']
+    }
+  }
+}
